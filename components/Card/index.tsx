fd408362df88355a4f6ce67fa5312e31116ff808
MonguDykrai <40307989+MonguDykrai@users.noreply.github.com>
2023-01-10 17:46:22 +0800

    封装组件，安装react-draggable

---
+++ components/Card/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardSteps from './CardSteps';
+
+const Card = () => {
+  return (
+    <div>
+      <CardSteps />
+    </div>
+  );
+};
+
+export default Card;
--- demo.tsx
+++ demo.tsx
@@ -1,68 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './index.css';
-import { Steps, Button } from 'antd';
+import Card from './components/Card';
 
 const App: React.FC = () => {
-  const [current, setCurrent] = useState(0);
-
-  const onChange = (value: number) => {
-    console.log('value:', value);
-    console.log('onChange:', current);
-    setCurrent(value);
-  };
-  const description = 'This is a description.';
-
   return (
     <>
-      <Steps
-        current={current}
-        onChange={onChange}
-        items={[
-          {
-            // 标题
-            title: '连线',
-            // 子标题
-            // subTitle: 'subTitle1',
-            description: (
-              <>
-                {/* draw icon */}
-                <Button>开始</Button>
-                {/* finish icon */}
-                <Button>结束</Button>
-                {/* clear icon */}
-                <Button>重置</Button>
-              </>
-            ),
-            // 指定状态
-            // status: 'wait',
-            // status: 'process',
-            // status: 'finish',
-            // status: 'error',
-          },
-          {
-            title: '图标',
-            // subTitle: 'subTitle2',
-            description: (
-              <>
-                {/* draw icon */}
-                <Button>设置</Button>
-                {/* clear icon */}
-                <Button>重置</Button>
-              </>
-            ),
-          },
-          {
-            title: '完成',
-            // subTitle: 'subTitle3',
-            description: (
-              <>
-                <Button>保存</Button>
-                <Button>取消</Button>
-              </>
-            ),
-          },
-        ]}
-      />
+      <Card />
     </>
   );
 };
